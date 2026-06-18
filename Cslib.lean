d0c864f76b37cace3a8067f3be665499b82dc535
lengyijun <sjtu5140809011@gmail.com>
2026-06-18 14:06:37 +0800

    wip: still a lot unproved

--- Cslib.lean
+++ Cslib.lean
@@ -182,6 +182,9 @@ public import Cslib.Languages.LambdaCalculus.LocallyNameless.Untyped.Properties
 public import Cslib.Languages.LambdaCalculus.LocallyNameless.Untyped.Size
 public import Cslib.Languages.LambdaCalculus.LocallyNameless.Untyped.StandardReduction
 public import Cslib.Languages.LambdaCalculus.LocallyNameless.Untyped.StrongNorm
+public import Cslib.Languages.LambdaCalculus.LocallyNameless.Untyped.Abstract
+public import Cslib.Languages.LambdaCalculus.LocallyNameless.Untyped.TakahashiSupport
+public import Cslib.Languages.LambdaCalculus.LocallyNameless.Untyped.Takahashi
 public import Cslib.Languages.LambdaCalculus.Named.Untyped.Basic
 public import Cslib.Languages.LambdaCalculus.Named.Untyped.Properties
 public import Cslib.Languages.Mech.Choreography.Basic
