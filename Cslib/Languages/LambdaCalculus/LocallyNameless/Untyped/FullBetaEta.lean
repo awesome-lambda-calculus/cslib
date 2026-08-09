55f86468f2c36edde20cb1784729f0ba08a1dac1
lengyijun <sjtu5140809011@gmail.com>
2026-08-10 05:58:23 +0800

    fix after rebase

--- Cslib/Languages/LambdaCalculus/LocallyNameless/Untyped/FullBetaEta.lean
+++ Cslib/Languages/LambdaCalculus/LocallyNameless/Untyped/FullBetaEta.lean
@@ -37,23 +37,42 @@ abbrev FullBetaEta : Term Var → Term Var → Prop := FullBeta ⊔ FullEta
 
 namespace FullBetaEta
 
-theorem redex_app_l_cong (redex : M ↠βηᶠ M') (lc_N : LC N) : app M N ↠βηᶠ app M' N := by
-  induction redex with
+variable {M M' N N' : Term Var}
+
+theorem step_app_l_cong (step : M ⭢βηᶠ M') (lc_N : LC N) : app M N ⭢βηᶠ app M' N := by
+    rcases step with h | h
+    · exact join_inl (h.appR lc_N)
+    · exact join_inr (h.appR lc_N)
+
+theorem step_app_r_cong (step : M ⭢βηᶠ M') (lc_N : LC N) : app N M ⭢βηᶠ app N M' := by
+    rcases step with h | h
+    · exact join_inl (h.appL lc_N)
+    · exact join_inr (h.appL lc_N)
+
+theorem steps_app_l_cong (steps : M ↠βηᶠ M') (lc_N : LC N) : app M N ↠βηᶠ app M' N := by
+  induction steps with
   | refl => grind
   | tail _ h ih =>
-    refine .tail ih ?_
+    refine ih.tail ?_
     rcases h with h | h
     · exact join_inl (h.appR lc_N)
     · exact join_inr (h.appR lc_N)
 
-theorem redex_app_r_cong (redex : M ↠βηᶠ M') (lc_N : LC N) : app N M ↠βηᶠ app N M' := by
-  induction redex with
+theorem steps_app_r_cong (steps : M ↠βηᶠ M') (lc_N : LC N) : app N M ↠βηᶠ app N M' := by
+  induction steps with
   | refl => grind
   | tail _ h ih =>
-    refine .tail ih ?_
+    refine ih.tail ?_
     rcases h with h | h
     · exact join_inl (h.appL lc_N)
     · exact join_inr (h.appL lc_N)
+
+lemma steps_fv [HasFresh Var] [DecidableEq Var] (steps : M ↠βηᶠ N) : N.fv ⊆ M.fv := by
+  induction steps with
+  | refl => grind
+  | tail _ h _ => cases h with
+    | inl h => grind [FullBeta.step_not_fv h]
+    | inr h => grind [FullEta.step_not_fv h]
 
 theorem from_beta (redex : M ↠βᶠ M') :  M ↠βηᶠ M' := by grind
 
